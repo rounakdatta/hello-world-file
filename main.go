3171bb92e7f1f19d33fd30ed5171aad19fbf1eb5
Rounak Datta <rounakdatta12@gmail.com>
2023-01-06 12:18:49 +0530

    Update formatting

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"flag"
 	"fmt"
 	"io/ioutil"
@@ -10,22 +11,39 @@ import (
 	"path/filepath"
 )
 
+func applyJsonIndentation(contents map[string]interface{}) string {
+	output, err := json.MarshalIndent(contents, "", "\t")
+	if err != nil {
+		return ""
+	}
+	return string(output)
+}
+
+func formatFileContent(fileContent string) map[string]interface{} {
+	contentJson := make(map[string]interface{})
+	_ = json.Unmarshal([]byte(fileContent), &contentJson)
+	delete(contentJson, "valueTypeUrn")
+	delete(contentJson, "hidden")
+	return contentJson
+}
+
 func readAllFilesToStringFromDirectory(directoryPath string) string {
 	files, err := ioutil.ReadDir(directoryPath)
 	if err != nil {
 		return "DIRECTORY INVALID"
 	}
 
-	directoryContents := ""
+	directoryContents := make(map[string]interface{})
 	for _, file := range files {
 		if !file.IsDir() {
 			fileContent, err := ioutil.ReadFile(filepath.Join(directoryPath, file.Name()))
 			if err == nil {
-				directoryContents += fmt.Sprintf("%s\n---\n%s\n\n", file.Name(), fileContent)
+				directoryContents[file.Name()] = formatFileContent(string(fileContent))
+
 			}
 		}
 	}
-	return directoryContents
+	return applyJsonIndentation(directoryContents)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
